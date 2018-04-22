77bf8ad28c83e1dcf340e862de9c7c148fceb379
Bapusaheb Patil <bapoo1998@gmail.com>
2018-04-22 18:48:16 +0530

    Initial Commit

---
+++ cgproject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cgproject
+//
+//  Created by Bapusaheb Patil on 22/04/18.
+//  Copyright © 2018 Bapusaheb Patil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
