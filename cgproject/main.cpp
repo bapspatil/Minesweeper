decb5ee4f290bcdd60523b815767856df50613a0
Bapusaheb Patil <bapoo1998@gmail.com>
2018-04-22 19:35:00 +0530

    Commit0

--- cgproject/main.cpp
+++ cgproject/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+#include <stdio.h>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cout << "Hello, World!\n";
+    return 1;
 }
